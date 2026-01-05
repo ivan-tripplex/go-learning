ff5ccef589ad45b030445e62967bead30d5d41ea
Иван Ширшов <ivan@Ivans-MacBook-Air.local>
2026-01-05 15:56:58 +0400

    feat: update calculation

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello, Git!")
+	var x, y, i float64
+	var op string
+
+	fmt.Println("Enter first number:")
+	fmt.Scan(&x)
+	fmt.Println("Enter operation (+,-,/,*,sqrt,**,ln)")
+	fmt.Scan(&op)
+	fmt.Println("Enter second number:")
+	fmt.Scan(&y)
+	res := operations(x, y, op)
+	fmt.Printf("result: %f\n", res)
+	for op != "0" {
+		fmt.Println("Enter operation (+,-,/,*,sqrt,**,ln)")
+		fmt.Scan(&op)
+		if op != "end" {
+			fmt.Println("Enter next number:")
+			fmt.Scan(&i)
+			res = operations(res, i, op)
+			fmt.Printf("result: %f\n", res)
+		} else {
+			break
+		}
+	}
+
+}
+
+func operations(x float64, y float64, op string) float64 {
+	switch op {
+	case "+":
+		return x + y
+	case "-":
+		return x - y
+	case "*":
+		return x * y
+	case "/":
+		return x / y
+	case "sqrt":
+		return math.Sqrt(x)
+	case "**":
+		return math.Pow(x, y)
+	case "ln":
+		return math.Log(x)
+	default:
+		fmt.Println("Invalid operation")
+		return 0
+	}
 }
